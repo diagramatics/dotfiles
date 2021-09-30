b255c2559dac05c805a068cb80f8a984d14b598f
Steven Sinatra <steven@stevensinatra.id>
2021-09-30 16:29:09 +1000

    Updates

--- .dotfiles-files/.deps.sh
+++ .dotfiles-files/.deps.sh
@@ -27,6 +27,7 @@ add-zsh-hook chpwd load-nvmrc
 load-nvmrc
 
 # Nix
+export PATH="/nix/var/nix/profiles/default/bin:$PATH"
 if [ -e "$HOME/.nix-profile/etc/profile.d/nix.sh" ]; then . "$HOME/.nix-profile/etc/profile.d/nix.sh"; fi # added by Nix installer
 
 # Cargo/rust
@@ -35,8 +36,23 @@ export PATH="$HOME/.cargo/bin:$PATH"
 # Enable github autocompletion
 eval "$(gh completion --shell zsh)"
 
-# pyenv
-if command -v pyenv 1>/dev/null 2>&1; then
-  eval "$(pyenv init -)"
-fi
+# (The below instructions are intended for common
+# shell setups. See the README for more guidance
+# if they don't apply and/or don't work for you.)
+
+# Add pyenv executable to PATH and
+# enable shims by adding the following
+# to ~/.profile and ~/.zprofile:
+
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+
+# Load pyenv into the shell by adding
+# the following to ~/.zshrc:
+
+eval "$(pyenv init -)"
+
+# Make sure to restart your entire logon session
+# for changes to profile files to take effect.
 
--- deps.sh
+++ deps.sh
@@ -18,13 +18,13 @@ brew install vim --override-system-vi
 sh <(curl -L https://nixos.org/nix/install) --darwin-use-unencrypted-nix-store-volume
 brew cask install iterm2
 brew install pyenv
+brew install git-delta
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | sh
 
 nvm install node --reinstall-packages-from=node
 nvm alias default node
 npm install -g avn avn-nvm avn-n
-npm install -g diff-so-fancy
 npm install -g yarn
 
 # Rust
