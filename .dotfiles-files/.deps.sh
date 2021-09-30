e4f5bb09b4c4ec807650885b82b4715524736794
Steven Sinatra <steven@stevensinatra.id>
2021-09-30 16:30:26 +1000

    Updates

--- .dotfiles-files/.deps.sh
+++ .dotfiles-files/.deps.sh
@@ -48,11 +48,3 @@ export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init --path)"
 
-# Load pyenv into the shell by adding
-# the following to ~/.zshrc:
-
-eval "$(pyenv init -)"
-
-# Make sure to restart your entire logon session
-# for changes to profile files to take effect.
-
